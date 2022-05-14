1992f5499e8d406077fdd25230a8fd89dfd3ca81
mohamedtamer0 <mt4452242@gmail.com>
2022-05-14 03:57:34 +0200

    update

--- BookListRazor/BulkyBook/BulkyBookWeb/Program.cs
+++ BookListRazor/BulkyBook/BulkyBookWeb/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-//builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
