c94793369a2c88805b81cde2539c2037f49bc883
mohamedtamer0 <mt4452242@gmail.com>
2022-05-14 04:55:26 +0200

    update

--- BookListRazor/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BookListRazor/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -42,5 +42,44 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
         }
+
+
+
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id ==0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomeError", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
     }
 }
