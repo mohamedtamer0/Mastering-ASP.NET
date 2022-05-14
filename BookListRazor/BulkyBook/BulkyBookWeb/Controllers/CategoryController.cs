6f5d5e61865d307a46e0979c992fa9b81643e9e9
mohamedtamer0 <mt4452242@gmail.com>
2022-05-14 04:40:46 +0200

    update

--- BookListRazor/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BookListRazor/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -18,5 +18,29 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomeError", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
--- BookListRazor/BulkyBook/BulkyBookWeb/Models/Category.cs
+++ BookListRazor/BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -9,7 +10,8 @@ namespace BulkyBookWeb.Models
 
         [Required]
         public string Name { get; set; }
-
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!!")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
