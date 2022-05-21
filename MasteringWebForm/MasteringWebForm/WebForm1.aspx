2baffee77f436846f47c7d44ea7f8c60691fe246
Mohamed Tamer <mohamed.tamer@elsewedy.com>
2022-05-21 21:48:17 +0200

    update

---
+++ MasteringWebForm/MasteringWebForm/WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="MasteringWebForm.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Button" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ MasteringWebForm/MasteringWebForm/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MasteringWebForm
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        int s = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                TextBox1.Text = "0";
+            }
+            Button1.Click += delegate
+            {
+                if (ViewState["click"] != null)
+                {
+                    s = (int)ViewState["click"] + 1;
+                }
+
+                TextBox1.Text = s.ToString();
+
+                ViewState["click"] = s;
+            };
+
+        }
+    }
+}
